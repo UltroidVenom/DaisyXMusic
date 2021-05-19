e666d4d9b4822c277311a828dbbb547e2cdc05ed
UltroidVenom <80195199+UltroidVenom@users.noreply.github.com>
2021-05-19 13:21:34 +0530

    Update pmpermit.py

--- DaisyXMusic/modules/pmpermit.py
+++ DaisyXMusic/modules/pmpermit.py
@@ -24,6 +24,7 @@ from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
     await USER.send_message(
-        message.chat.id,
-       "Hi there, This is the music assistant service of @VenomMusicBot .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **IF YOU ARE UNABLE TO ADD ASSISTANT OR HAVE PROBLEM REGARDING MUSIC BOT KINDLY CONTACT @R2K_VENOM** \n\n **⚠️ Note : if you are unable to add music assistant in  private groups just make the group public for one minute and send the group link here or in @R2K_VENOM dm and after the music assistant joins you can make your group private again** \n\n **❗if you need any help just ask here @CrackMonkey**")
+       message.chat.id,
+        "Hi there, This is the music assistant service of @VenomMusicBot .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **IF YOU ARE UNABLE TO ADD ASSISTANT OR HAVE PROBLEM REGARDING MUSIC BOT KINDLY CONTACT @R2K_VENOM** \n\n **⚠️ Note : if you are unable to add music assistant in  private groups just make the group public for one minute and send the group link here or in @R2K_VENOM dm and after the music assistant joins you can make your group private again** \n\n **❗if you need any help just ask here @CrackMonkey**",
+    )
   return
